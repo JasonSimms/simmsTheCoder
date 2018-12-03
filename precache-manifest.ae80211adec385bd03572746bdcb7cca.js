e26bbd487b3c2468a628432af087de4b1492cf6f
gitgut0110 <jsimms0110@gmail.com>
2018-12-03 15:51:17 +0100

    Updates

---
+++ precache-manifest.ae80211adec385bd03572746bdcb7cca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/simmsthecoder/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "268950a46f5462e609ec89bec5a0d7fc",
+    "url": "/simmsthecoder/static/media/jason1.268950a4.jpg"
+  },
+  {
+    "revision": "dc90776029c87faae5ee",
+    "url": "/simmsthecoder/static/js/runtime~main.dc907760.js"
+  },
+  {
+    "revision": "13873e7aded135d1234f",
+    "url": "/simmsthecoder/static/js/main.13873e7a.chunk.js"
+  },
+  {
+    "revision": "a9d6ba5203de63d53915",
+    "url": "/simmsthecoder/static/js/1.a9d6ba52.chunk.js"
+  },
+  {
+    "revision": "13873e7aded135d1234f",
+    "url": "/simmsthecoder/static/css/main.97d51719.chunk.css"
+  },
+  {
+    "revision": "09c253f24c56264086b02f5c36663dff",
+    "url": "/simmsthecoder/index.html"
+  }
+];
